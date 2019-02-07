d8b341aa02d70e2cd3ab39a6a05b1288ff090d73
AVGasanov <avgasanov@gmail.com>
2019-02-07 22:58:01 +0400

    Add solution for "AOP Around Advice Type"
    - Update Course lecture

---
+++ spring-demo-aop-zz-around/src/com/luv2code/aopdemo/AfterFinallyDemoApp2.java
@@ -0,0 +1,40 @@
+package com.luv2code.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+
+public class AfterFinallyDemoApp2 {
+
+	public static void main(String[] args) {
+		
+		// read spring configuration java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// call method to find the accounts
+		List<Account> theAccounts = null;
+		
+		try {
+			boolean tripWire=true;
+			theAccounts = theAccountDAO.findAccounts(tripWire);
+		} catch (Exception e) {
+			System.out.println("\n Main Program exception" + e.getMessage());
+		}
+		
+		// display the accounts
+		System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+		System.out.println("-----------");
+		
+		System.out.println(theAccounts);
+		
+		// close the context
+		context.close();
+	}
+
+}
