978adae3ff40e2d9c4cec0ee60cfee48d4e9d5c4
AVGasanov <avgasanov@gmail.com>
2019-02-12 00:56:41 +0400

    Add solution for 67-68th lessons
    - Update Course lecture

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,9 +3,11 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +48,26 @@ public class CustomerRestController {
 		customerService.saveCustomer(customer);
 		
 		return customer;
+	}
+	
+	@PutMapping("/customers") 
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	@DeleteMapping("/customers")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer customer = customerService.getCustomer(customerId);
+		
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer Id not found - " + customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "Deleted customer id - " + customerId;
 	}
 }
