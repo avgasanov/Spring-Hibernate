ace838349db9ef40fc2ead612a46ed5e6cc1a962
AVGasanov <avgasanov@gmail.com>
2019-02-13 01:21:13 +0400

    Add solution for "Spring Boot - Application Properties"
    - Update Course lecture

---
+++ 05-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolappsboot/rest/FunRestController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springboot.demo.mycoolappsboot.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	@GetMapping("/teamInfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + ", Team name: " + teamName;
+	}
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time in server is " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5 k";
+	}
+	
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "It is your lucky day";
+	}
+}
---
+++ 05-properties-demo/src/test/java/com/luv2code/springboot/demo/mycoolappsboot/MycoolappSbootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.luv2code.springboot.demo.mycoolappsboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MycoolappSbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
