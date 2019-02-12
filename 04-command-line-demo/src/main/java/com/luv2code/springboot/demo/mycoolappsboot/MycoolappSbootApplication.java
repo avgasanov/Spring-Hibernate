58e0a1434133ba1f96a3cb6de38760a86e1ec88b
AVGasanov <avgasanov@gmail.com>
2019-02-13 00:58:37 +0400

    Add solution for "Spring Boot - Application Properties"
    - Update Course lecture

---
+++ 04-command-line-demo/src/main/java/com/luv2code/springboot/demo/mycoolappsboot/MycoolappSbootApplication.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot.demo.mycoolappsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappSbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappSbootApplication.class, args);
+	}
+
+}
+
